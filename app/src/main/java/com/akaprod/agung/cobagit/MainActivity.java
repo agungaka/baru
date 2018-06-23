0ff10e8f62a856e778a6831c4aa2c07cc6dc5f50
DESKTOP-O90QIGB\agung <agung.aka@gmail.com>
2018-06-23 16:27:41 +0700

    satu

---
+++ app/src/main/java/com/akaprod/agung/cobagit/MainActivity.java
@@ -0,0 +1,13 @@
+package com.akaprod.agung.cobagit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
