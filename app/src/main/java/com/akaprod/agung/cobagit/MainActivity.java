4abb1172e0f9d38fc1e90d65ac8eef301f6ca509
DESKTOP-O90QIGB\agung <agung.aka@gmail.com>
2018-06-23 16:42:23 +0700

    update 1

--- app/src/main/java/com/akaprod/agung/cobagit/MainActivity.java
+++ app/src/main/java/com/akaprod/agung/cobagit/MainActivity.java
@@ -2,12 +2,18 @@ package com.akaprod.agung.cobagit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    
+    Button btnHello;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        btnHello = (Button) findViewById(R.id.btnHello);
+        
     }
 }
